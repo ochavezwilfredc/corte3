2d75bd9c471966e6784cb887540f11185d4b2622
ochavezwilfredc <ochavezwilfredc@crece.uss.edu.pe>
2019-05-19 13:14:36 -0500

    cambios finales ok

--- vista/footer.php
+++ vista/footer.php
@@ -60,9 +60,6 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
 
-<!-- (Optional) Latest compiled and minified JavaScript translation files -->
-<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>-->
-
 <!--Fecha-->
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
